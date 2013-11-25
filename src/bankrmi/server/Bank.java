9efe6235b165b4f55f74bf2f8622baebe22e125e
Mikael Tenhunen <mikael.tenhunen@gmail.com>
2013-11-25 12:15:00 +0100

    start

---
+++ src/bankrmi/server/Bank.java
@@ -0,0 +1,12 @@
+package bankrmi.server;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Set;
+
+public interface Bank extends Remote {
+    public Account newAccount(String name) throws RemoteException, RejectedException;
+    public Account getAccount(String name) throws RemoteException;
+    public boolean deleteAccount(String name) throws RemoteException;
+    public String[] listAccounts() throws RemoteException;
+}
