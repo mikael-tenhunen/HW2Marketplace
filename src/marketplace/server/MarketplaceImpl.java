f0fb13767604e468eff66cff23abe22893f315fd
kalle936 <kalle936@hotmail.com>
2013-11-26 17:31:39 +0100

    DEYTOOKYOURJEEERB

--- src/marketplace/server/MarketplaceImpl.java
+++ src/marketplace/server/MarketplaceImpl.java
@@ -1,13 +1,27 @@
 package marketplace.server;
 
+import bankrmi.shared.Account;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import marketplace.shared.Marketplace;
+import marketplace.shared.MarketplaceAccount;
 
 public class MarketplaceImpl extends UnicastRemoteObject implements Marketplace {
     
     public MarketplaceImpl() throws RemoteException {
         super();
         
+    }
+
+    @Override
+    public MarketplaceAccount registerCustomer(String customerName, Account bankAccount) throws RemoteException {
+    }
+
+    @Override
+    public boolean unregisterCustomer(String customerName) throws RemoteException {
+    }
+
+    @Override
+    public listItems() throws RemoteException {
     }
 }
